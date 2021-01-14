50e9f13de152e33a608034507fe65ab0c36bbf81
ESGit3 <ethanshan706@gmail.com>
2021-01-14 11:56:48 -0500

    fixed radix sort simple

--- Radix.java
+++ Radix.java
@@ -9,8 +9,11 @@ public class Radix {
     }
 
     public static int nth(int n, int col) throws IndexOutOfBoundsException {
-        if (col < 0 || col >= length(n)) {
+        if (col < 0) {
             throw new IndexOutOfBoundsException(col + " index is out of bounds");
+        }
+        if (col >= length(n)) {
+            return 0;
         }
         if (n < 0) {
             n = -n;
@@ -20,8 +23,32 @@ public class Radix {
     }
 
     public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
-        for (MyLinkedList i : buckets) {
+        for (SortableLinkedList i : buckets) {
             original.extend(i);
+        }
+    }
+
+    public static void radixSortSimple(SortableLinkedList data) {
+        if (data.size() < 2) {
+            return;
+        }
+        SortableLinkedList[] buckets = new SortableLinkedList[10];
+        SortableLinkedList temp = new SortableLinkedList();
+        int max = 0;
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i) > max) {
+                max = data.get(i);
+            }
+        }
+
+        for (int i = 0; i < length(max); i++) {
+            for (int j = 0; j < data.size(); j++) {
+                buckets[nth(data.get(i), i)].add(data.get(i));
+            }
+            while (data.size() > 0) {
+                data.remove(0);
+            }
+            merge(data, buckets);
         }
     }
 }
